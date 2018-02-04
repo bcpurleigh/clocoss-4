f52e6ef5a354b94069301b8f674c8d96cd63292a
Benjamin Campbell <ben@polar.digital>
2018-02-04 16:04:11 +0000

    Added comments

--- cron.sh
+++ cron.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#Perform the automatic backup
 gcloud beta datastore export --namespaces='up730061s4' gs://bucket-730061/
--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Make the bucket if it does not already exist
 gsutil mb -c regional -l europe-west2 gs://bucket-730061
+
+# Perform the first backup
 gcloud beta datastore export --namespaces='up730061s4' gs://bucket-730061/
